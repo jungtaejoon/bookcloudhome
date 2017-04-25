7f06efd87677c31d71a4062168832fbac8e3008d
jungtaejoon <jungtaejoon@naver.com>
2017-04-26 02:44:48 +0900

    Initial commit

---
+++ src/main/java/com/bookcloudpub/BookcloudhomeApplication.java
@@ -0,0 +1,12 @@
+package com.bookcloudpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcloudhomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcloudhomeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bookcloudpub/BookcloudhomeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookcloudpub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookcloudhomeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
